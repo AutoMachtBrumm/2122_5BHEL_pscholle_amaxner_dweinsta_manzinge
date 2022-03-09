5dbf6ea462212eac2f60d6a1797823d3107b1d66
Davidoff535 <dweinstabl@gmail.com>
2022-03-09 12:17:20 +0100

    JSON

--- Server/src/main/java/ClientThread.java
+++ Server/src/main/java/ClientThread.java
@@ -1,21 +1,46 @@
+import Fragen.Frage;
+
 import java.io.*;
 import java.net.Socket;
 import java.util.Random;
 import java.util.Vector;
 
-public class ClientThread extends Thread{
+public class ClientThread extends Thread {
     private Socket socket;
     Vector<Befragung> befragungen;
+    Befragung befragung;
+
     public ClientThread(Socket socket, Vector<Befragung> befragungen) {
         this.socket = socket;
-        this.befragungen=befragungen;
+        this.befragungen = befragungen;
     }
+
     @Override
     public void run() {
         try {
             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
-            String befragung=reader.readLine();
+            do {
+                String befname = reader.readLine();
+
+                for (Befragung bef :
+                        befragungen) {
+                    if (bef.getName().equals(befname)) {
+                        befragung = bef;
+                        break;
+                    }
+                }
+                if (befragung == null) {
+                    writer.write("Befragung nicht gefunden\n");
+                    writer.flush();
+                }
+            } while (befragung == null);
+
+
+            for (Frage frage:
+                 befragung.getFragen()) {
+                writer.write();
+            }
 
             socket.close();
         } catch (IOException e) {
