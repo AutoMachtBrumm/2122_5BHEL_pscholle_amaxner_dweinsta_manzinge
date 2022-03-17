2df25ae0f07c0db22e1acf5c07995a7110d4c399
michaelanzinger <michael@anzcom.at>
2022-03-17 12:13:56 +0100

    Client Überarbeitung

--- Client/src/main/java/client/Client.java
+++ Client/src/main/java/client/Client.java
@@ -3,76 +3,92 @@ package client;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
-import java.util.Objects;
 
 public class Client {
 
-    private Socket socket = null;
-    private BufferedReader reader = null;
-    private BufferedWriter writer = null;
+    private Socket socket;
 
-    public Client(InetAddress address, int port){
-
+    public Client(InetAddress address, int port) {
         try {
-            // Creates Socket for Client & Buffers
-            connectToServer(address, port);
 
-            // Send Authentication Key
-            sendDataToServ("Befragung1");
+            // Create Socket
+            this.socket = new Socket(address, port);
+            // Starts ClientThread
+            new ClientThread(socket).start();
 
-            String jsonString = "";
-
-            // Repeat process until Server send "ENDE"
-            while(jsonString.equals("ENDE")) {
-                // Receive --> JSON String form Server
-                jsonString = getDataFromServ();
-                System.out.println(jsonString);
-                String answer = QuestionHandler.editJSON(jsonString);
-                sendDataToServ(answer);
-            }
-
-            // Close Socket
-            closeServerConnection();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+}
 
-    private void connectToServer(InetAddress address, int port) throws IOException {
-        socket = new Socket(address, port);
-        // Create Buffers
-        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+class ClientThread extends Thread {
+
+    private final Socket socket;
+    private final BufferedReader reader;
+    private final BufferedWriter writer;
+
+    private boolean isRunning = false;
+
+    public ClientThread(Socket s) throws IOException {
+        this.socket = s;
+        this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
     }
 
-    private void closeServerConnection() throws IOException {
-        if (!socket.isClosed()) {
-            reader.close();
-            writer.close();
-            socket.close();
-        }
-    }
-
-    private String getDataFromServ() {
-        StringBuilder inputString = new StringBuilder();
+    @Override
+    public void run() {
+        // Starts ClientThread
+        isRunning = true;
         try {
-            String line;
-            while ((line = reader.readLine()) != null) {
-                inputString.append(line);
+            // Send Authentication Key
+            sendDataToServ("Befragung1");
+
+            String response;
+            while (isRunning) {
+                // Response --> Question
+                response = getDataFromServ();
+                System.out.println("Server: " + response);
+
+                if (response.equals("ENDE")) {
+                    isRunning = false;
+                    System.out.println("Client-Server Connection closed");
+                    break;
+                }
+                String answer = QuestionHandler.editJSON(response);
+                System.out.println("Client: " + answer);
+                sendDataToServ(answer);
             }
-            return inputString.toString();
         } catch (IOException ex) {
             System.out.println("I/O error: " + ex.getMessage());
+        } finally {
+            closeServerConnection();
         }
-        return null;
     }
 
-    private void sendDataToServ(String json) {
+    private void closeServerConnection() {
         try {
-            writer.write(json);
-            writer.flush();
+            if (!socket.isClosed()) {
+                reader.close();
+                writer.close();
+                socket.close();
+            }
         } catch (IOException ex) {
             System.out.println("I/O error: " + ex.getMessage());
         }
+    }
+
+    private String getDataFromServ() throws IOException {
+        StringBuilder inputString = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            inputString.append(line);
+        }
+        return inputString.toString();
+    }
+
+    private void sendDataToServ(String json) throws IOException {
+        writer.write(json);
+        writer.flush();
     }
 }
