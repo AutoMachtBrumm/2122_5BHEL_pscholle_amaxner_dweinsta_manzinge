06d4acee515f29d2bc81c0f340487a0d73779479
Davidoff535 <dweinstabl@gmail.com>
2022-03-23 11:33:43 +0100

    Javafx Maven Dependency hinzugefügt

--- Server/src/main/java/Main.java
+++ Server/src/main/java/Main.java
@@ -3,22 +3,39 @@ import Fragen.FrageBool;
 import Fragen.FrageNum;
 import Fragen.FrageText;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
 import javax.swing.text.html.HTMLDocument;
 import java.sql.SQLException;
 import java.util.Iterator;
 import java.util.Vector;
 import java.util.stream.Collectors;
 
-public class Main {
 
-    public static void main(String[] args) {
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+
+        ImageView imageView = new ImageView();
+        HBox hbox = new HBox(imageView);
+        Scene scene = new Scene(hbox);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
         Server server = new Server();
 
-        Befragung befragung=new Befragung(1,"Befragung2");
-        befragung.addFrage(new FrageText(0,1,10,"Wie gehts?"));
-        befragung.addFrage(new FrageBool(0,1,10,"Alles gut?"));
-        befragung.addFrage(new FrageNum(0,1,10,"Wie alt?",0,120));
+        Befragung befragung = new Befragung(1, "Befragung2");
+        befragung.addFrage(new FrageText(0, 1, 10, "Wie gehts?"));
+        befragung.addFrage(new FrageBool(0, 1, 10, "Alles gut?"));
+        befragung.addFrage(new FrageNum(0, 1, 10, "Wie alt?", 0, 120));
         server.befragungen.add(befragung);
         server.startServer();
+
     }
 }
